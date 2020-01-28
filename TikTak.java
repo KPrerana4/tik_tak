2abfe03c683389a9a0d535a5d67d95039502ac44
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-01-28 14:55:06 +0530

    add more methods to TikTak

--- TikTak.java
+++ TikTak.java
@@ -2,99 +2,104 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class TikTak
 {
-    static Character[][] tik=new Character[3][3];
+    private Character[][] grid=new Character[3][3];
+    int no_of_inputs;
+    
 	public static void main(String[] args) 
 	{
-		Scanner sc=new Scanner(System.in);
 		boolean f=false;
-		int ip=0;
+		TikTak obj=new TikTak();
 		System.out.println("Player 1:X\t\tPlayer 2:O");
+		int count=0;
+		short player_no;
 		
 		while(true)
 		{
-		  while(true)
-		  {
-		    System.out.println("\nPlayer 1, Enter position(as x y):");
-		    int x1=sc.nextInt();
-		    int y1=sc.nextInt();
-		    if(tik[x1][y1]==null && x1<3 && y1<3)
-		    {
-		        ip++;
-		        tik[x1][y1]='X';
-		        f=check(x1,y1);      
-		           break;
-		    }
-		  }
+		  player_no=2;
+		  if(count%2==0)
+		     player_no=1;
+		  f=obj.take_input(player_no);
 		  for(int i=0;i<3;i++)
-		     System.out.println(Arrays.toString(tik[i]));
-		  if(f==true){System.out.println("\nPLAYER 1 WON");
+		     System.out.println(Arrays.toString(obj.grid[i]));
+		  if(f==true){System.out.println("\nPLAYER "+player_no+" WON");
 		     break;}
 		     
-		  if(ip==9){
+		  if(obj.no_of_inputs==9){
 		      System.out.println("Game over,No one won");
 		      break;
 		   }
-		   
-		   
-		   
-		  while(true)
-		  {
-		     System.out.println("\nPlayer 2, Enter position(as x y):");
-		     int x2=sc.nextInt();
-		     int y2=sc.nextInt();
-		     if(tik[x2][y2]==null && x2<3 && y2<3)
-		     {ip++;
-		       tik[x2][y2]='O';    
-		       f=check(x2,y2);     
-		        break;
-		     }
-		  } 
-		  for(int i=0;i<3;i++)
-		     System.out.println(Arrays.toString(tik[i]));
-		  if(f==true){System.out.println("\nPLAYER 2 WON");
-		     break;}
-		     
-		   if(ip==9){
-		      System.out.println("Game over,No one won");
-		      break;
-		   }
+		   count++;
 	   }
 	}
 	
 	
-	static boolean check(int x,int y)
+    boolean check_row_coloum(int x,int y)
 	{
-	    Character c=tik[x][y];
+	    Character c=grid[x][y];
 	    int f1=1,i,j;
 	            
 	    for(i=0;i<3;i++)
-	       if(tik[x][i]!=c)
+	       if(grid[x][i]!=c)
 	          f1=0;
-	             
-	    if(f1==1)return true;
+	    if(f1==1)
+	        return true;
+	    
 	    f1=1;
 	    for(i=0;i<3;i++)
-	       if(tik[i][y]!=c)
+	       if(grid[i][y]!=c)
 	           f1=0;
-	           
 	   if(f1==1)
 	      return true;
-	   f1=1;
+	      
+	   return false;
+	}
+	
+	
+	boolean validate_diagonal_pos(int x,int y)
+	{
+	   Character c=grid[x][y];
+	   int i,j,f1=1;
 	   for(i=0,j=0;i<3;i++,j++)
-	      if(tik[i][j]!=c)
+	      if(grid[i][j]!=c)
 	         f1=0;
 	   if(f1==1)
 	      return true;
 	          
 	   f1=1;
 	   for(i=0,j=2;i<3;i++,j--)
-	      if(tik[i][j]!=c)
+	      if(grid[i][j]!=c)
 	         f1=0;
 	   if(f1==1)
 	      return true;
-	          
-	   
+	         
 	   return false;
 	}
-
+	
+	
+    boolean take_input(short player_no)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    boolean row_coloum=false,diagonal=false;
+	    Character c='O';
+	    if(player_no==1)
+	        c='X';
+	    while(true)
+		  {
+		    System.out.println("\nPlayer "+player_no+", Enter position(as x y):");
+		    int row_pos=sc.nextInt();
+		    int coloum_pos=sc.nextInt();
+		    if(row_pos<3 && coloum_pos<3 && grid[row_pos][coloum_pos]==null)
+		    {
+		        no_of_inputs++;
+		        grid[row_pos][coloum_pos]=c;
+		        row_coloum=check_row_coloum(row_pos,coloum_pos); 
+		        diagonal=validate_diagonal_pos(row_pos,coloum_pos);
+		           break;
+		    }
+		  }
+		  if(row_coloum || diagonal)
+		      return true; 
+		  return false;
+	}
 }
+
