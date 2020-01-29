65a660e58c9eec8292721425aa770b193e698f46
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-01-29 14:11:11 +0530

    other approach to TikTak

--- TikTak.java
+++ TikTak.java
@@ -1,108 +1,86 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 public class TikTak
 {
-    private Character[][] grid=new Character[3][3];
-    int no_of_valid_inputs;    
+	String[] all_success_positions={"123","456","789","147","258","369","159","357"};
+	String[] player=new String[2];
+	int filled_positions=0;
+	Character[] placed=new Character[9];
 
 	public static void main(String[] args) 
 	{
-		boolean game_win=false;
-		TikTak game_obj=new TikTak();
-		System.out.println("Player 1:X\t\tPlayer 2:O");
-		int count=0;
-		short player_no;
-		
+		byte player_no,count=0;
+		TikTak game=new TikTak();
 		while(true)
 		{
-		   player_no=2;
-		   if(count%2==0)
-		      player_no=1;
-		   game_win=game_obj.start_game(player_no);
-		   for(int i=0;i<3;i++)
-		      System.out.println(Arrays.toString(game_obj.grid[i]));
-		   if(game_win==true){System.out.println("\nPLAYER "+player_no+" WON");
-		      break;}
-		     
-		   if(game_obj.no_of_valid_inputs==9){
-		       System.out.println("Game over,No one won");
-		       break;
-		    } 
-		    count++;
-	   }
+			player_no=1;
+			if(count%2==0)
+				player_no=0;
+			if(game.input(player_no))
+			{
+				System.out.println("PLAYER "+(player_no+1)+" WON");
+				break;
+			}
+			game.printGrid();
+			count++;
+			if(game.filled_positions==9)
+			  { 
+			  	System.out.println("Game Over,No one Won");
+			  	break;
+			  }
+		}
 	}
-	
-		
-    boolean start_game(short player_no)
+
+
+	void printGrid()
 	{
-	    Scanner scanner_obj=new Scanner(System.in);
-	    boolean row_coloum=false,diagonal=false;
-	    Character c='O';
-	    if(player_no==1)
-	        c='X';
-	    while(true)
-		  {
-		    System.out.println("\nPlayer "+player_no+", Enter position(as x y):");
-		    int row_pos=scanner_obj.nextInt();
-		    int coloum_pos=scanner_obj.nextInt();
-		    if(row_pos<3 && coloum_pos<3 && grid[row_pos][coloum_pos]==null)
-		    {
-		        no_of_valid_inputs++;
-		        grid[row_pos][coloum_pos]=c;
-		        ValidateGame validate_obj=new ValidateGame();
-		        row_coloum=validate_obj.check_row_coloum(row_pos,coloum_pos,grid); 
-		        diagonal=validate_obj.validate_diagonal_pos(row_pos,coloum_pos,grid);
-		           break;
-		    }
-		  }
-		  if(row_coloum || diagonal)
-		      return true; 
-		  return false;
+		for(int pos=0;pos<9;pos++)
+			{
+				System.out.print(placed[pos]+" ");
+				if(pos==2 || pos==5 || pos==8)
+					System.out.println();
+			}
+	}
+
+
+	boolean input(int player_no)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Player "+(player_no+1)+"'s turn:");
+		int val;
+		byte win=0;
+		Character c='X';
+		if(player_no==1)
+			 c='O';
+		while(true)
+		{
+			val=sc.nextInt();
+			if(placed[val-1]==null && val<10)
+			{	
+				filled_positions++;
+				player[player_no]+=Integer.toString(val);
+				placed[val-1]=c;
+				return (new gameCheck().check(player[player_no],all_success_positions));
+			}
+		}
+	}		
+}
+
+class gameCheck
+{
+	boolean check(String player,String[] all_success_positions)
+	{
+	   int flag;
+		for(int each_success=0;each_success<all_success_positions.length;each_success++)
+		{
+			flag=1;
+			for(int pos=0;pos<all_success_positions[each_success].length();pos++)
+				 if(!player.contains(Character.toString(all_success_positions[each_success].charAt(pos))))
+				 	 flag=0;
+			if(flag==1)
+				return true;
+		}
+		return false;	
 	}
 }
 
-
-
-class ValidateGame
-{
- boolean check_row_coloum(int row_pos,int coloum_pos,Character[][] grid)
- {
-    Character c=grid[row_pos][coloum_pos];
-	    int win=1,i,j;
-	            
-	    for(i=0;i<3;i++)
-	       if(grid[row_pos][i]!=c)
-	          win=0;
-	    if(win==1)
-	        return true;
-	    
-	    win=1;
-	    for(i=0;i<3;i++)
-	       if(grid[i][coloum_pos]!=c)
-	           win=0;
-	   if(win==1)
-	      return true;
-	      
-	   return false;
-   }
-
-   boolean validate_diagonal_pos(int row_pos,int coloum_pos,Character[][] grid)
-   {
-   	  Character c=grid[row_pos][coloum_pos];
-	   int i,j,win=1;
-	   for(i=0,j=0;i<3;i++,j++)
-	      if(grid[i][j]!=c)
-	         win=0;
-	   if(win==1)
-	      return true;
-	          
-	   win=1;
-	   for(i=0,j=2;i<3;i++,j--)
-	      if(grid[i][j]!=c)
-	         win=0;
-	   if(win==1)
-	      return true;
-	         
-	   return false;
-   }
-  }
